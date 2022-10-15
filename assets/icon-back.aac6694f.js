725ce07797ba9bbd8ef6300ece5a1a72a90bf7fb
youchunchen <youchun719@gmail.com>
2022-10-16 02:12:39 +0800

    更新bms/pcs的information

---
+++ assets/icon-back.aac6694f.js
@@ -0,0 +1,1 @@
+import{o as e,a as o,d as t}from"./index.b2a61821.js";const s={viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg"},c=t("path",{d:"M8 11.8 16.32 7v9.6L8 11.8Z"},null,-1),n=[c];function a(r,d){return e(),o("svg",s,n)}const i={render:a};export{i as default,a as render};
